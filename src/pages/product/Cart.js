f8235de8714f18bf454b2ae4320e57972371040c
mr_oroz <oroz7307@gmail.com>
2021-02-28 12:28:47 +0600

    delete useState

--- src/pages/product/Cart.js
+++ src/pages/product/Cart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect, Fragment } from 'react'
+import React, { useContext, Fragment } from 'react'
 import { DataContext } from '../ProductProvider'
 import { Link } from 'react-router-dom'
 import Rouble from "../../assets/images/ruble-sign-solid.svg"
