08f7e7fad1bead2b13577569e26cf2559a5f5af3
mr_oroz <oroz7307@gmail.com>
2021-04-03 14:43:07 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import Main from './pages/Main';
 import "./App.scss"
-import { ProductProvider } from './pages/ProductProvider';
+import {ProductProvider} from './pages/ProductProvider';
 import Header from './components/Header';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter, BrowserRouter as Router} from 'react-router-dom'
 
 
 function App() {
-  return (
-    <ProductProvider>
-      <div className='App'>
-        <Router>
-          <Header />
-          <Main />
-        </Router>
-      </div>
-    </ProductProvider>
-  );
+    return (
+        <BrowserRouter >
+            <ProductProvider>
+                <div className='App'>
+                    <Router>
+                        <Header/>
+                        <Main/>
+                    </Router>
+                </div>
+            </ProductProvider>
+        </BrowserRouter>
+    );
 }
 
 export default App;
